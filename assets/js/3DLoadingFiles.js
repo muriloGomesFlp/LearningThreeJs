19ff62ca7c57bdbc9b4eb4737d4e182144d4ed15
Murilo s. Gomes <lilo.flp@gmail.com>
2021-11-17 15:47:57 -0300

    done! just send with dropzone dont work, but it only remains to configure the server-side. Nov 17, 21

--- assets/js/3DLoadingFiles.js
+++ assets/js/3DLoadingFiles.js
@@ -1,41 +1,20 @@
 import * as THREE from '../three.js-master/build/three.module.js'
 import { GLTFLoader } from '../three.js-master/examples/jsm/loaders/GLTFLoader.js'
 import { OrbitControls } from '../three.js-master/examples/jsm/controls/OrbitControls.js'
-import { GUI } from '../three.js-master/examples/jsm/libs/dat.gui.module.js';
+import * as TESTE from '../js/darkModeController.js'
+let controls, renderer, scene, camera, loader
 
+function showModels(modeloSelected) {
+    showElements(modeloSelected);
 
-let controls, renderer, scene, camera
-
-function init(modeloSelected) {
-    $('#showCanvas').append('<canvas class="showModel"></canvas>');
     `Para carregar qualquer modelo 3D utilizando o Three é necessário três elementos principais, scene, camera e o render`
     //set a new scene
     scene = new THREE.Scene()
 
     //GLTFLoader carrega o modelo 3D
-    var model3d = new THREE.Object3D()
-    const loader = new GLTFLoader();
-    loader.load(modeloSelected, function (gltf) {
-        //criar o objeto3d
-        model3d = gltf.scene
-        //define o tamanho "scala"
-        model3d.scale.set(0.05, 0.05, 0.05)
-        //centralizar o objeto na tela
-        const box3 = new THREE.Box3().setFromObject(model3d);       
-        const vector = new THREE.Vector3();
-        box3.getCenter(vector);
-        model3d.position.set(-vector.x, -vector.y, -vector.z);
+    carregarModelo(modeloSelected)
 
-        //add o objeto ao cenário
-        scene.add(model3d)
-    }, function (xhr) {
-        $('#progressBar').append(`<div class="progress-bar bg-primary" role="progressbar"
-        aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: `+ (xhr.loaded / xhr.total * 100).toFixed(2) + `%">
-    </div>`)
-        //console.error((xhr.loaded / xhr.total * 100).toFixed(0) + '% loaded');
-    }, function (err) {
-        console.log('An error')
-    });
+    
 
     //tamanho do elemento html usado para renderizar o objeto, neste caso esta usando toda a janela
     let box = document.querySelector('#colCanvas');
@@ -81,30 +60,99 @@ function init(modeloSelected) {
     animate()
 }
 
-
 //a função animação permite controlar a camera, ações para o objeto3D etc...
 function animate() {
     requestAnimationFrame(animate)
     controls.update()
     renderer.render(scene, camera)
 }
-//função para criar um painel
+//função para carregar os modelos, add a scene e continuar o loading da função showModels
+function carregarModelo(modeloSelected) {
+    var model3d = new THREE.Object3D()
+    loader = new GLTFLoader();
 
+    loader.load(modeloSelected, function (gltf) {
+        //criar o objeto3d
+        model3d = gltf.scene
+        //define o tamanho "scala"
+        model3d.scale.set(0.05, 0.05, 0.05)
+        //centralizar o objeto na tela
+        const box3 = new THREE.Box3().setFromObject(model3d);
+        const vector = new THREE.Vector3();
+        box3.getCenter(vector);
+        model3d.position.set(-vector.x, -vector.y, -vector.z);
+
+        //add o objeto ao cenário
+        scene.add(model3d)
+    }, function (xhr) {
+        $('#progressBar').append(`<div class="progress-bar bg-primary" role="progressbar"
+        aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: `+ (xhr.loaded / xhr.total * 100).toFixed(2) + `%">
+    </div>`)
+        //console.error((xhr.loaded / xhr.total * 100).toFixed(0) + '% loaded');
+    }, function (err) {
+        console.log('An error')
+    });
+
+}
+//coletar o nome dos diretório + arquivos e disponibilizar no dropdown na tela viewModel
+function getModelDir() {
+    let folderName = []
+
+    $.getJSON('../static/3dModels', folders => {
+        $.each(folders, function (i, direcotry) {
+            folderName.push(direcotry);
+            $.getJSON('../static/3dModels/' + direcotry, function (folder) {
+                $.each(folder, function (x, file) {
+                    if (file.split('.').pop() == 'glb' || file.split('.').pop() == 'gltf') {
+                        $('#modelsAvalible').append($('<option>', {
+                            value: '../static/3dModels/' + folderName[i] + '/' + file,
+                            text: file.split('.').shift().toUpperCase()
+                        }));
+                    }
+
+                });
+            });
+        });
+    });
+    
+}
+
+function showElements(nomeModelo) {
+        
+    $('#showCanvas').append('<canvas class="showModel"></canvas>');
+    $('#accordion').css('display', 'initial');
+    $('#detalhes').append(`<div class="card-body">
+    <p><b>Nome modelo:</b>`+nomeModelo+`</p><br>
+    <p><b>Data Criação:</b>2020-04-03</p><br>
+    <p><b>Criador:</b>Robert</p><br>                                     
+    </div>`);
+    $('#info').append(`<div class="card-body">
+    <p>Licença Free</p>                                  
+    </div>`);
+}
 
 $(function () {
+
+    getModelDir();
+
     $('#formSelectModels').click(function () {
         const modelSelect = $('#formSelectModels').find(":selected").val()
         if (modelSelect == '') {
             $('#showCanvas').empty()
             $('#progressBar').empty()
+            $('#detalhes').empty()
+            $('#info').empty()
+
             alert('Selecione um Modelo')
         } else {
             try {
+                $('#detalhes').empty()
+                $('#info').empty()
                 $('#showCanvas').empty()
                 $('#progressBar').empty()
             }
             finally {
-                init(modelSelect)
+                showModels(modelSelect)
             }
         }
     })
--- assets/js/darkModeController.js
+++ assets/js/darkModeController.js
@@ -1,27 +1,33 @@
 $(function () {
-    $('.darkmode').click(function () {
-        let mode = $('.darkmode').attr('data-widget')
 
+    $('.darkmode').click(function () {
+        let mode = $('.darkmode').attr('data-widget');
         switch (mode) {
             case 'light':
-                $('.darkmode').empty()
-                $('.darkmode').append('<i class="fas fa-moon"></i>')
-                $('.darkmode').attr('data-widget', 'dark')
-                $('body').addClass('dark-mode')
-                $('nav').removeClass('navbar-white navbar-light')
-                $('nav').addClass('navbar-dark')
-               
-                break
+                $('.darkmode').empty();
+                $('.darkmode').append('<i class="fas fa-moon"></i>');
+                $('.darkmode').attr('data-widget', 'dark');
+                $('body').addClass('dark-mode');
+                $('nav').removeClass('navbar-white navbar-light');
+                $('nav').addClass('navbar-dark');
+                break;
 
             case 'dark':
-                $('.darkmode').empty()
-                $('.darkmode').append('<i class="fas fa-sun"></i>')
-                $('.darkmode').attr('data-widget', 'light')
-                $('body').removeClass('dark-mode')
-                $('nav').removeClass('navbar-dark')
-                $('nav').addClass('navbar-white navbar-light')
-                break
-        }
-        $
-    })
-})
+                $('.darkmode').empty();
+                $('.darkmode').append('<i class="fas fa-sun"></i>');
+                $('.darkmode').attr('data-widget', 'light');
+                $('body').removeClass('dark-mode');
+                $('nav').removeClass('navbar-dark');
+                $('nav').addClass('navbar-white navbar-light');
+                break;
+            default:
+                $('.darkmode').empty();
+                $('.darkmode').append('<i class="fas fa-sun"></i>');
+                $('.darkmode').attr('data-widget', 'light');
+                $('body').removeClass('dark-mode');
+                $('nav').removeClass('navbar-dark');
+                $('nav').addClass('navbar-white navbar-light');
+                break; 
+        };
+    });
+});
